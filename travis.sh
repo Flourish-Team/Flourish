e706cead37054918e16d6eff7e92a5916e8dc801
Andrew Thorpe <pyroka@gmail.com>
2016-02-20 22:51:49 +0000

    More source

--- travis.sh
+++ travis.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
 echo "Building Premake"
-cd Premake/source/build/gmake.unix
-make
+cd Premake/source
+make -f Bootstrap.mak $TRAVIS_OS_NAME
+bin/release/premake5 test
 echo "Building Flourish"
 cd ../../../
 chmod +x ./source/bin/premake5
