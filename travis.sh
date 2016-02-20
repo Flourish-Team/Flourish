14ed7c0861a1cd8630779e009ec9e564aa9f1514
Andrew Thorpe <pyroka@gmail.com>
2016-02-20 23:07:03 +0000

    Add config to the path of premake

--- travis.sh
+++ travis.sh
@@ -7,8 +7,8 @@ make -f Bootstrap.mak $TRAVIS_OS_NAME
 bin/release/premake5 test
 echo "Building Flourish"
 cd ../../../
-chmod +x ./source/bin/premake5
-./source/bin/premake5 make
+chmod +x ./source/bin/release/premake5
+./source/bin/release/premake5 make
 cd ../Projects/make
 make
 make test
