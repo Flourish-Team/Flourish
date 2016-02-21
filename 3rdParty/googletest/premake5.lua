deab5c7e84b04e9da9d701af1b5151370e347286
Andrew Thorpe <pyroka@gmail.com>
2016-02-21 01:04:49 +0000

    Turn off pthread

--- 3rdParty/googletest/premake5.lua
+++ 3rdParty/googletest/premake5.lua
@@ -2,6 +2,7 @@ project "GoogleTest"
    kind "StaticLib"
    language "C++"
    targetdir "bin/%{cfg.buildcfg}"
+   defines { "GTEST_HAS_PTHREAD=0" }
 
    files { "include/gtest/**.h", "src/**.cc", "scr/**.h" }
    includedirs { "include", "." }
