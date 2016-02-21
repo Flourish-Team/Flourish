2746a61afe9c3cded981a7d257a4b7a403fde8d4
Andrew Thorpe <pyroka@gmail.com>
2016-02-21 01:24:34 +0000

    Turn off notifications. Build/Test release too.

--- travis.sh
+++ travis.sh
@@ -10,7 +10,15 @@ cd ../
 chmod +x ./source/bin/release/premake5
 ./source/bin/release/premake5 gmake
 cd ../Projects/gmake
-make
+make config=$BUILD_TYPE
 cd ../../
-chmod +x ./Bin/Debug/TestProject
-./Bin/Debug/TestProject
+
+if[ "$BUILD_TYPE" = "debug" ] then
+  chmod +x ./Bin/Debug/TestProject
+  ./Bin/Debug/TestProject
+elif [ "$BUILD_TYPE" = "release" ] then
+  chmod +x ./Bin/Debug/TestProject
+  ./Bin/Debug/TestProject
+else
+  echo "Unknown build type $BUILD_TYPE"
+fi
