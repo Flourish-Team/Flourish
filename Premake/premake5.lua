299a519936619066099a87d46563abbba8c3c574
Andrew Thorpe <pyroka@gmail.com>
2016-02-21 11:25:31 +0000

    Attempt to generate symbols on OSX release

--- Premake/premake5.lua
+++ Premake/premake5.lua
@@ -3,6 +3,10 @@ workspace "Flourish"
    configurations { "Debug", "Release" }
    location("../Projects/" .. _ACTION)
    flags { "Unicode" }
+   -- Stripping symbols from OSX in release causes an error in LD
+   if os.get() == "macosx" then
+    flags { "Symbols" }
+   end
 
 -- include "../Libs/Core"
 include "../Libs/Test"
