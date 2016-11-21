e3985f162a0b07a9622776f698ca809c178c0686
Andrew Thorpe <pyroka@gmail.com>
2016-11-21 21:52:11 +0000

    Basic implementation of ParallelFor

---
+++ Libs/Core/Include/Task/ParallelFor.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <functional>
+
+#include "Task/Task.h"
+
+namespace Flourish
+{
+    template<typename DataType, typename Splitter, typename TaskSystem>
+    void ParallelForFunc(TaskId parentTask, DataType* data, int32_t dataCount, Splitter* splitter, std::function<void(DataType*,uint32_t)> workFunc, TaskSystem* taskSystem)
+    {
+        if(!splitter->ShouldSplit(data, dataCount))
+        {
+            auto taskId = taskSystem->BeginAdd(taskSystem->WorkItemWithTaskAllocator([&](void*){
+                workFunc(data, dataCount);
+            }));
+            taskSystem->AddChild(parentTask, taskId);
+            taskSystem->FinishAdd(taskId);
+        }
+        else
+        {
+            auto leftSize = dataCount / 2u;
+            ParallelForFunc(parentTask, data, leftSize, splitter, workFunc, taskSystem);
+            ParallelForFunc(parentTask, data + leftSize, dataCount - leftSize, splitter, workFunc, taskSystem);
+        }
+    }
+    
+    template<typename DataType, typename Splitter, typename TaskSystem = class TaskManager>
+    class ParallelFor
+    {
+        typedef std::function<void(DataType*,uint32_t)> WorkFunc;
+    public:
+        ParallelFor(DataType* data, uint32_t dataCount, Splitter* splitter, WorkFunc workFunc, TaskSystem* taskSystem)
+            : _data(data)
+            , _dataCount(dataCount)
+            , _splitter(splitter)
+            , _workFunc(workFunc)
+            , _taskSystem(taskSystem)
+        {
+        }
+        
+        TaskId Run()
+        {
+            auto taskId = _taskSystem->BeginAdd(WorkItem::Empty());
+            ParallelForFunc(taskId, _data, _dataCount, _splitter, _workFunc, _taskSystem);
+            _taskSystem->FinishAdd(taskId);
+            return taskId;
+        }
+        
+    private:
+        DataType* _data;
+        uint32_t _dataCount;
+        Splitter* _splitter;
+        WorkFunc _workFunc;
+        TaskSystem* _taskSystem;
+    };
+}
