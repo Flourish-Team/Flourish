27184c50f41d43872d78f7afaf718f3f74bb4ac3
Andrew Thorpe <pyroka@gmail.com>
2017-02-19 23:46:36 +0000

    Added '##' infront of all the __VA_ARGS__ in Memory.h, which removes the trailing comma if there are no arguments (this might not work in VS)

--- Libs/Core/Include/Memory/Memory.h
+++ Libs/Core/Include/Memory/Memory.h
@@ -12,11 +12,11 @@
 
 //Alocates a object of type T and returns an unmanaged raw pointer. Has to be freed manually with FL_DELETE_RAW()
 #define FL_NEW_RAW(allocator, type, ...) \
-	Flourish::Memory::Internal::NewRawPointer<type>((allocator), alignof(type), MAKE_SOURCE_INFO, __VA_ARGS__)
+    Flourish::Memory::Internal::NewRawPointer<type>((allocator), alignof(type), MAKE_SOURCE_INFO, ##__VA_ARGS__)
 
 //Alocates a object of type T, aligned to alignment, and returns an unmanaged raw pointer. Has to be freed manually with FL_DELETE_RAW()
 #define FL_NEW_RAW_ALIGN(allocator, type, alignment, ...) \
-	Flourish::Memory::Internal::NewRawPointer<type>((allocator), (alignment), MAKE_SOURCE_INFO, __VA_ARGS__)
+	Flourish::Memory::Internal::NewRawPointer<type>((allocator), (alignment), MAKE_SOURCE_INFO, ##__VA_ARGS__)
 
 //Deletes a raw pointer allocated with FL_NEW_RAW/FL_NEW_RAW_ALIGN
 #define FL_DELETE_RAW(allocator, ptr) \
@@ -26,21 +26,21 @@
 
 //Alocates a object of type T and returns an unique_ptr to the object. Will be automaticly cleaned up once out of scope using the provided deleter
 #define FL_NEW_UNIQUE(allocator, type, ...) \
-	Flourish::Memory::Internal::NewUniquePtr<type>((allocator), alignof(type), MAKE_SOURCE_INFO, __VA_ARGS__)
+	Flourish::Memory::Internal::NewUniquePtr<type>((allocator), alignof(type), MAKE_SOURCE_INFO, ##__VA_ARGS__)
 
 //Alocates a object of type T, aligned to alignment, and returns an unique_ptr to the object. Will be automaticly cleaned up once out of scope using the procided deleter
 #define FL_NEW_UNIQUE_ALIGN(allocator, type, alignment, ...) \
-	Flourish::Memory::Internal::NewUniquePtr<type>((allocator), (alignment), MAKE_SOURCE_INFO, __VA_ARGS__)
+	Flourish::Memory::Internal::NewUniquePtr<type>((allocator), (alignment), MAKE_SOURCE_INFO, ##__VA_ARGS__)
 
 
 
 //Alocates a object of type T and returns an shared_ptr to the object. Will be automaticly cleaned up once out of scope using the provided deleter
 #define FL_NEW_SHARED(allocator, type, ...) \
-	Flourish::Memory::Internal::NewSharedPtr<type>((allocator), alignof(type), __VA_ARGS__)
+	Flourish::Memory::Internal::NewSharedPtr<type>((allocator), alignof(type), ##__VA_ARGS__)
 
 //Alocates a object of type T, aligned to alignment, and returns an shared_ptr to the object. Will be automaticly cleaned up once out of scope using the procided deleter
 #define FL_NEW_SHARED_ALIGN(allocator, type, alignment, ...) \
-	Flourish::Memory::Internal::NewSharedPtr<type>((allocator), (alignment), __VA_ARGS__)
+	Flourish::Memory::Internal::NewSharedPtr<type>((allocator), (alignment), ##__VA_ARGS__)
 
 
 
